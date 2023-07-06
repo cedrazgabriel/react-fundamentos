d036d68b62e9b470a6019b60e0021f5acfbfb2ea
Gabriel Cedraz <69488235+cedrazgabriel@users.noreply.github.com>
2023-07-06 20:56:18 -0300

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+ReactDOM.render(
+    <h1 id='title'>
+        <span>Olá World!!!</span>
+    </h1>,
+    document.getElementById('root'),
+)
+
+
