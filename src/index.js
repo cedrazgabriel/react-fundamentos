a55696a9a21523422bdd9489f78e3fab0d637c07
Gabriel Cedraz <69488235+cedrazgabriel@users.noreply.github.com>
2023-07-07 22:40:32 -0300

    Adicionado loader do babel

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom'
 
 ReactDOM.render(
     <h1 id='title'>
-        <span>Olá World!!!</span>
+        <span>Olá Julia!!!</span>
     </h1>,
     document.getElementById('root'),
 )
