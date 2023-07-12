7c0cb4e1685da59f97060906e16ad1dac6855c40
Gabriel Cedraz <69488235+cedrazgabriel@users.noreply.github.com>
2023-07-11 21:42:17 -0300

    add saass depedencies

--- src/Header.js
+++ src/Header.js
@@ -2,7 +2,7 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import Button from './Button'
 
-import styles from './Header.css'
+import styles from './Header.scss'
 
 export default function Header(props){
     return (
